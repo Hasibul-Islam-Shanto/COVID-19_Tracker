638948ff63fa2a9e7ddd1f4ff3523cb18f7e5bb4
Hasibul-Islam-Shanto <mdhasibulislamshanto007@gmail.com>
2022-04-17 22:36:35 +0600

    done by shanto

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,77 +1,83 @@
-import React,{useState, useEffect} from 'react';
-import {fetchDataDaily} from '../api/api';
-import {Line, Bar} from 'react-chartjs-2';
+import React, { useState, useEffect } from "react";
+import { fetchDataDaily } from "../api/api";
 import { Chart as ChartJS } from "chart.js/auto";
+import { Line, Bar } from "react-chartjs-2";
 
-const Chart = ({info :{confirmed, recovered, deaths}, country}) => {
-  
-    const [dailyData, setDailyData] = useState({});
+const Chart = ({ info: { confirmed, recovered, deaths }, country }) => {
+  const [dailyData, setDailyData] = useState({});
 
-    useEffect(()=>{
-        const dailyData = async () =>{
-            setDailyData(await fetchDataDaily());
-        }
-        dailyData();
-    },[])
+  useEffect(() => {
+    const dailyData = async () => {
+      setDailyData(await fetchDataDaily());
+    };
+    dailyData();
+  }, []);
 
-    const lineChart = dailyData[0] ? (
-      <Line
-        data={{
-          labels: dailyData.map(({ date }) => date),
-          datasets: [
-            {
-              data: dailyData.map((data) => data.confirmed),
-              label: "Infected",
-              borderColor: "#3333ff",
-              fill: true,
-            },
-            {
-              data: dailyData.map((data) => data.deaths),
-              label: "Deaths",
-              borderColor: "red",
-              backgroundColor: "rgba(255, 0, 0, 0.5)",
-              fill: true,
-            },
-            {
-              data: dailyData.map((data) => data.recovered),
-              label: "Recovered",
-              borderColor: "green",
-              backgroundColor: "rgba(0, 255, 0, 0.5)",
-              fill: true,
-            },
-          ],
-        }}
-      />
-    ) : null;
+  const lineChart = dailyData[0] ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map((data) => data.confirmed),
+            label: "Infected",
+            borderColor: "#3333ff",
+            fill: true,
+          },
+          {
+            data: dailyData.map((data) => data.deaths),
+            label: "Deaths",
+            borderColor: "red",
+            backgroundColor: "rgba(255, 0, 0, 0.5)",
+            fill: true,
+          },
+          {
+            data: dailyData.map((data) => data.recovered),
+            label: "Recovered",
+            borderColor: "green",
+            backgroundColor: "rgba(0, 255, 0, 0.5)",
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
 
-    const barChart = (
-      confirmed ?
-      (<Bar
-        data={{
-          labels: ['Infected', 'Recovered', 'Deaths'],
-          datasets: [
-            {
-              label: 'People',
-              backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
-              data: [confirmed.value, recovered.value, deaths.value],
-            },
-          ],
-        }}
-        options={{
-          legend: { display: false },
-          title: { display: true, text: `Current state in ${country}` },
-        }}
-      />) : null
-    );
+  const barChart = confirmed ? (
+    <Bar
+      data={{
+        labels: ["Infected", "Recovered", "Deaths"],
+        datasets: [
+          {
+            label: "People",
+            backgroundColor: [
+              "rgba(0, 0, 255, 0.5)",
+              "rgba(0, 255, 0, 0.5)",
+              "rgba(255, 0, 0, 0.5)",
+            ],
+            data: [confirmed.value, recovered.value, deaths.value],
+          },
+        ],
+      }}
+      options={{
+        legend: { display: false },
+        title: { display: true, text: `Current state in ${country}` },
+      }}
+    />
+  ) : null;
   return (
     <>
-      <div style={{
-          width:"60%",
-          height:"auto",
-          marginTop:"1rem"
-      }}>{country ? barChart : lineChart}</div>
+      <div
+        style={{
+          width: "60%",
+          height: "auto",
+          marginTop: "1rem",
+        }}
+      >
+        {country ? barChart : lineChart}
+      </div>
     </>
   );
-}
+};
 
-export default Chart
+export default Chart;
